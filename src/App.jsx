b02596304064abca256593331c3297de1604d38c
Byaxy <charlesbyaxy@gmail.com>
2024-11-12 08:25:30 +0000

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="flex items-center justify-center h-screen">
+    <h1 className="text-4xl font-bold">Certificate Verification System</h1>
+  </div>
+);
+
+export default App;
